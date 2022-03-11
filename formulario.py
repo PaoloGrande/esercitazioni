13c508ffb371a4a585aec745c7737c717b6b7778
PaoloGrande <paolo.grande69@gmail.com>
2022-03-11 12:57:14 +0100

    aggiunta formulario

---
+++ formulario.py
@@ -0,0 +1,8 @@
+
+def perimetrotriang(l1,l2,l3):  #calcola il perimetro di un triangolo dai i lati
+  perimt=l1+l2+l3
+  return 'Perimetro Triangolo = ', perimt
+
+def areatriangolo(b,h):     #calcola l'area di un triangolo dati base e altezza
+  areat = b*h/2
+  return 'Area Triangolo =', areat
